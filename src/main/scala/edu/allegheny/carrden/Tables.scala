ef44affbc2146c58758e55526b1df4236b3012bc
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-27 18:18:19 -0500

    Schema for a produce table

---
+++ src/main/scala/edu/allegheny/carrden/Tables.scala
@@ -0,0 +1,20 @@
+package edu.allegheny.carrden
+
+import scala.slick.driver.H2Driver.simple._
+/**
+ * Created by hawk on 1/27/15.
+ */
+object Tables {
+
+  class Produce(tag: Tag) extends Table[(String,Int,Double)](tag,"PRODUCE") {
+    def name  = column[String]("NAME", O.PrimaryKey)
+    def count = column[Int]("COUNT")
+    def price = column[Double]("PRICE")
+
+    // Every table needs a * projection with the same type as the table's type parameter
+    def * = (name,count,price)
+  }
+
+  val suppliers = TableQuery[Produce]
+
+}

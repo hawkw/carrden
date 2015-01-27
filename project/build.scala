1295aea91d80b7bf7d7786c979b1df472f827178
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-27 13:44:04 -0500

    Add Slick dependency to sbt

--- project/build.scala
+++ project/build.scala
@@ -11,6 +11,7 @@ object CarrdenInventoryBuild extends Build {
   val Version = "0.0.0"
   val ScalaVersion = "2.11.4"
   val ScalatraVersion = "2.3.0"
+  val SlickVersion = "3.0.0"
 
   lazy val project = Project (
     "carrden-inventory",
@@ -22,13 +23,14 @@ object CarrdenInventoryBuild extends Build {
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
       libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "container",
-        "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0"
+        "org.scalatra"        %%  "scalatra"          % ScalatraVersion,
+        "org.scalatra"        %%  "scalatra-scalate"  % ScalatraVersion,
+        "org.scalatra"        %%  "scalatra-specs2"   % ScalatraVersion   % "test",
+        "com.typesafe.slick"  %%  "slick"             % SlickVersion,
+        "ch.qos.logback"      %   "logback-classic"   % "1.1.2"           % "runtime",
+        "org.eclipse.jetty"   %   "jetty-webapp"      % "9.1.5.v20140505" % "container",
+        "org.eclipse.jetty"   %   "jetty-plus"        % "9.1.5.v20140505" % "container",
+        "javax.servlet"       %   "javax.servlet-api" % "3.1.0"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(

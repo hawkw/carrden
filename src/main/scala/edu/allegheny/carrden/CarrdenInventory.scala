af8439557f1b481bd6c08cc3c0a373fe5015af0a
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-28 16:10:42 -0500

    Backend for handling a sale

--- src/main/scala/edu/allegheny/carrden/CarrdenInventory.scala
+++ src/main/scala/edu/allegheny/carrden/CarrdenInventory.scala
@@ -1,15 +1,23 @@
 package edu.allegheny.carrden
 
+import java.sql.Date
+
 import Tables._
 import org.scalatra._
+import org.scalatra.json._
+import org.json4s.{DefaultFormats, Formats}
 import scalate.ScalateSupport
 import scala.slick.driver.H2Driver.simple._
 import scala.slick.jdbc.JdbcBackend.Database.dynamicSession
 
-case class CarrdenInventory(db: Database) extends CarrdenInventoryStack {
+case class CarrdenInventory(db: Database) extends CarrdenInventoryStack with JacksonJsonSupport  {
+
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  case class Sale(sold: Map[String,Int])
+  case class SaleResult(price: Double)
 
   get("/") {
-    contentType="text/html"
     contentType="text/html"
     jade(
         "main",
@@ -26,7 +34,35 @@ case class CarrdenInventory(db: Database) extends CarrdenInventoryStack {
       produce.list.map { case (name, num, price) => s"We have $num $name and they cost $price dollars"} mkString "<br />"
     }
   }
-  
+
+  get("sale/") {
+    contentType=formats("json")
+    val date    = new Date(System.currentTimeMillis())
+    val sold    = parsedBody.extract[Sale].sold
+    db withDynTransaction { // start a new transaction
+      // (if 2+ point-of-sale clients are connected, we don't want
+      // them to try to add sales with the same number)
+      val saleNum = sales       // the sale number is equal to...
+        .filter(_.date == date) // get all sale records for today
+        .map(_.saleNum)         // extract the sale numbers
+        .list
+        .max +1                 // find the last sale number & increment
+      val processed = for { (item, count) <- sold }
+      yield { // generate the tuples to insert into the DB
+        ( date,
+          saleNum,
+          item,
+          count,
+          produce // get the current price for that produce item
+            .filter(_.name == item) // select the row for this item
+            .map(_.price)           // extract the price
+            .first * count          // multiply by the amount sold
+          )
+        }
+      sales ++= processed // insert rows into the DB
+      SaleResult(processed.map(_._5).sum) // reply to the client w/ the cost
+      }
+    }
 }
 case class CarrdenAdmin(db: Database) extends CarrdenInventoryStack {
 

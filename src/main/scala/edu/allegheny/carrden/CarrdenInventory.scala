daa4c4a3fe8a8359d1301ed3b50680b7d7d22c65
Hawk Weisman <hawk@meteorcodelabs.com>
2015-01-28 10:37:55 -0500

    Add demo JQuery form for main sale page

--- src/main/scala/edu/allegheny/carrden/CarrdenInventory.scala
+++ src/main/scala/edu/allegheny/carrden/CarrdenInventory.scala
@@ -1,5 +1,6 @@
 package edu.allegheny.carrden
 
+import Tables._
 import org.scalatra._
 import scalate.ScalateSupport
 import scala.slick.driver.H2Driver.simple._
@@ -9,7 +10,17 @@ case class CarrdenInventory(db: Database) extends CarrdenInventoryStack {
 
   get("/") {
     contentType="text/html"
-    jade("hello-scalate")
+    contentType="text/html"
+    jade("main", "inventory" -> db.withDynSession {
+      produce.list.map { case (name, num, price) => s"We have $num $name and they cost $price dollars"}
+    })
+  }
+
+  get("/produce") {
+    db withDynSession {
+      contentType = "text/html"
+      produce.list.map { case (name, num, price) => s"We have $num $name and they cost $price dollars"} mkString "<br />"
+    }
   }
   
 }
@@ -17,7 +28,24 @@ case class CarrdenAdmin(db: Database) extends CarrdenInventoryStack {
 
   get("/") {
     contentType="text/html"
-    jade("admin")
+    jade("admin", "inventory" -> db.withDynSession {
+      produce.list.map { case (name, num, price) => s"We have $num $name and they cost $price dollars"}
+    })
+  }
+
+  get("/db/create-tables/") {
+    db withDynSession { produce.ddl.create }
+  }
+
+  get("/db/load-test-data/") {
+    db withDynSession {
+      produce insertAll (
+        ("To-may-to", 30, 1.25),
+        ("To-mah-to", 30, 1.25),
+        ("Po-tay-to", 42, 3.50),
+        ("Po-tah-to", 42, 3.50)
+      )
+    }
   }
 
 }

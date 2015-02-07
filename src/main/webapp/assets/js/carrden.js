0e00b20baadcfb0cb6ff8f94abbc1494d3232f07
Hawk Weisman <hawk@meteorcodelabs.com>
2015-02-07 14:35:43 -0500

    Moved some common JS into its' own file

---
+++ src/main/webapp/assets/js/carrden.js
@@ -0,0 +1,23 @@
+/**
+ * Created by hawk on 2/7/15.
+ */
+function updateInventory() {
+    $.ajax({
+        type: 'get',
+        url: '/inventory',
+        data: '{}',
+        success: function (data, status) {
+            g_inventory = {}
+            for (var i in data) {
+                g_inventory[data[i]['name'].replace(' ', '_')] = {
+                    amount: data[i]['amount'],
+                    price: data[i]['price']
+                };
+            }
+            console.log("[Info] Got current inventory: " + g_inventory)
+        },
+        error: function (result) {
+            console.log("[Error] Something went wrong getting inventory: " + result)
+        }
+    });
+};
